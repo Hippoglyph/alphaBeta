ae7b58db25cf95f2079d8227cccf31dc54601012
Jonathan Rinnarv <rinnarv@kth.se>
2017-09-26 12:23:19 +0200

    Done with E-D

--- Cpp_skeletons/TTT3D/player.cpp
+++ Cpp_skeletons/TTT3D/player.cpp
@@ -4,8 +4,7 @@
 #include <limits>
 #include <cmath>
 
-//Author Marcus Lignercrona & Jonathan Rinnarv
-namespace TICTACTOE
+namespace TICTACTOE3D
 {
 int alphabeta(const GameState &state, int depth, int a, int b, bool pa){
 	int v = 2;
@@ -16,9 +15,35 @@ int alphabeta(const GameState &state, int depth, int a, int b, bool pa){
 			v = 1;
 		else if(state.isOWin()) 
 			v = -1;
-		else
+		else //if (state.isDraw())
 			v = 0;
-
+		/*else{
+			int score = 0;
+			for(int r = 0; r<4; ++r){
+				for(int c = 0; c<4; ++c){
+					if(r==c)
+						if(state.at(r,c)=='X')
+							score++;
+						else if(state.at(r,c)=='O')
+							score--;
+					if(3-r==c)
+						if(state.at(r,c)=='X')
+							score++;
+						else if(state.at(r,c)=='O')
+							score--;
+					if(state.at(r,c)=='X')
+						score+=2;
+					else if(state.at(r,c)=='O')
+						score-=2;
+				}
+			}
+			if(score == 0)
+				v = 0;
+			else if(score>0)
+				v = 1;
+			else
+				v = -1;
+		}*/
 	}
 	else if(pa){
 		v = std::numeric_limits<int>::min();
@@ -47,23 +72,26 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
 
     std::vector<GameState> lNextStates;
     pState.findPossibleMoves(lNextStates);
+
+    
     if (lNextStates.size() == 0) return GameState(pState, Move());
+
     int bestOption = std::numeric_limits<int>::min();
     int index = -1;
     for(int i = 0; i< lNextStates.size();++i){
-		int newVal= alphabeta(lNextStates[i],20,std::numeric_limits<int>::min(),std::numeric_limits<int>::max(),false);
+		int newVal= alphabeta(lNextStates[i],1,std::numeric_limits<int>::min(),std::numeric_limits<int>::max(),false);
 		if(std::max(bestOption,newVal)!=bestOption){
 			index = i;
 			bestOption = newVal;
 		}
 	}
 	return lNextStates[index];
-
     /*
      * Here you should write your clever algorithms to get the best next move, ie the best
      * next state. This skeleton returns a random move instead.
      */
+    
     //return lNextStates[rand() % lNextStates.size()];
 }
 
-/*namespace TICTACTOE*/ }
+/*namespace TICTACTOE3D*/ }
